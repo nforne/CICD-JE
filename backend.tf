a72d485b031fb4be1f304be56298ffa5e7fb2594
nforne <mnforne@gmail.com>
2021-03-01 19:41:15 -0800

    CICD-JE

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "cicd-tf"
     key    = "state/cicd-JE.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
     dynamodb_table = "tf-s3-x"
     profile = "NM-CE"
   }
